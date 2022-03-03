a6359822338e825a0ea6c2fde12a913b030e7622
Nabil Attia <attia.nabil@hotmail.com>
2022-03-03 16:25:13 +0100

    F_p

--- mandatory/converter.c
+++ mandatory/converter.c
@@ -6,7 +6,7 @@
 /*   By: nattia <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 08:19:56 by nattia            #+#    #+#             */
-/*   Updated: 2022/03/02 16:35:17 by nattia           ###   ########.fr       */
+/*   Updated: 2022/03/03 16:11:55 by nattia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	ft_converter(t_variable *so)
 	b = 0;
 	so->wall = mlx_xpm_file_to_image(so->mlx, "wall.xpm", &k, &b);
 	so->floor = mlx_xpm_file_to_image(so->mlx, "floor.xpm", &k, &b);
-	so->player = mlx_xpm_file_to_image(so->mlx, "satoshiplayer.xpm", &k, &b);
-	so->collect = mlx_xpm_file_to_image(so->mlx, "btc.xpm", &k, &b);
+	so->player = mlx_xpm_file_to_image(so->mlx, "player.xpm", &k, &b);
+	so->collect = mlx_xpm_file_to_image(so->mlx, "collectable.xpm", &k, &b);
 	so->exit = mlx_xpm_file_to_image(so->mlx, "exit.xpm", &k, &b);
 }
